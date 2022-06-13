947a2c18e8e791c2fde50011671a34ef5cb75f65
Vadim_kominch <vadzim.kominch@digitalmara.com>
2022-06-13 12:49:55 +0300

    feat: add some basic crud operations

--- src/repository/db.ts
+++ src/repository/db.ts
@@ -1,9 +1,11 @@
 import {User} from '../entity/User.js'
 
-class Db{
+export class Db{
     private static instance:Db
-
-    private constructor() {}
+    private users: Array<User>;
+    private constructor() {
+        this.users = new Array<User>()
+    }
 
     public static getInstance(): Db {
         if(!Db.instance) {
@@ -22,5 +24,15 @@ class Db{
         elements.push(new User())
         elements.push(new User())
         return elements
+    }
+
+    public saveOne(user: User|null) {
+        if(user != null) {
+            //save
+        }
+    }
+
+    public updateUser(id:string,user:User):User {
+        return user
     }
 }
